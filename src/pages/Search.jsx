958262e66fa6938988d3b9e5141b2d166a37e493
Allan Medeiros Minato <allan.minato@gmail.com>
2023-01-10 21:28:04 -0300

    Resolucao questao 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artistName: '',
+  };
+
   render() {
+    const { artistName } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          name=""
+          id=""
+          onChange={ ({ target: { value } }) => this.setState({
+            artistName: value,
+          }) }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ artistName.length < 2 }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
