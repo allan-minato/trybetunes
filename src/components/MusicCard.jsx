c4177c1eb7e26daafeb2e0c38797833cdd03e71b
Allan Medeiros Minato <allan.minato@gmail.com>
2023-01-13 18:45:42 -0300

    Resolucao requisito 7

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class MusicCard extends Component {
+  render() {
+    const { music } = this.props;
+    return (
+      <div>
+        <p>{ music.trackName }</p>
+        <audio data-testid="audio-component" src={ music.previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  previewUrl: PropTypes.string,
+  trackName: PropTypes.string,
+}.isRequired;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  state = {
+    album: [],
+    artistName: '',
+    collectionName: '',
+  };
+
+  componentDidMount() {
+    this.getAPI();
+  }
+
+  getAPI = async () => {
+    const { match } = this.props;
+    const musics = await getMusics(match.params.id);
+    this.setState({
+      album: musics.slice(1),
+      artistName: musics[0].artistName,
+      collectionName: musics[0].collectionName,
+    });
+  };
+
   render() {
+    const { album, artistName, collectionName } = this.state;
     return (
       <div data-testid="page-album">
-        <h2>página de Album</h2>
         <Header />
+        <p data-testid="album-name">{ collectionName }</p>
+        <p data-testid="artist-name">{ artistName }</p>
+        {album.map((music) => <MusicCard key={ music.trackId } music={ music } />)}
       </div>
     );
   }
 }
 
 export default Album;
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
