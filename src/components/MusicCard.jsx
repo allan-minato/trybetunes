7f3b90d3c779d883d2f296ffedfed51fc0be65ab
Allan Medeiros Minato <allan.minato@gmail.com>
2023-01-13 19:13:03 -0300

    Resolucao requisito 8

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 export default class MusicCard extends Component {
+  state = {
+    isLoading: false,
+  };
+
+  getApiSong = async () => {
+    const { music } = this.props;
+    this.setState({
+      isLoading: true,
+    });
+    await addSong(music);
+    this.setState({
+      isLoading: false,
+    });
+  };
+
   render() {
     const { music } = this.props;
+    const { isLoading } = this.state;
     return (
       <div>
+        { isLoading && <Loading /> }
         <p>{ music.trackName }</p>
         <audio data-testid="audio-component" src={ music.previewUrl } controls>
           <track kind="captions" />
@@ -15,6 +34,15 @@ export default class MusicCard extends Component {
           <code>audio</code>
           .
         </audio>
+        <label htmlFor="checkbox">
+          Favorita
+          <input
+            onChange={ this.getApiSong }
+            data-testid={ `checkbox-music-${music.trackId}` }
+            type="checkbox"
+            id="checkbox"
+          />
+        </label>
       </div>
     );
   }
